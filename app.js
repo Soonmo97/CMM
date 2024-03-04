9507c8da628c1312016cba461e12a83ab3a35378
soonmo <smkwon97@gmail.com>
2024-03-04 15:55:31 +0900

    긴급 수정

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(
     })
 );
 
-const resRouter = require("./routes/RestDetail");
+const resRouter = require("./routes/restDetail.js");
 const userRouter = require("./routes/user.js");
 const suggestRouter = require("./routes/suggestion.js");
 app.use("/restaurantDetail", resRouter);
